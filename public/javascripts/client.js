13142c3db0eca937fea36558ab148c89c0528d45
Joseph Volosin <josephvolosin@gmail.com>
2019-09-28 14:38:03 -0400

    Google Map renders

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,13 @@
 // Creates the Google Map (Thanks for the code, Google!)
 // source: https://developers.google.com/maps/documentation/javascript/adding-a-google-map
-function initMap() {
+var initMap = function() {
+    console.log("Creating initial map..");
     var umbcCoords = {lat:39.2558586, lng:-76.7116755};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: umbcCoords});
+        document.getElementById('map'), {zoom: 16, center: umbcCoords});
 }
 
 $(document).ready(function() {
-    console.log("Running.");
+    console.log("Running!");
+    initMap();
 });

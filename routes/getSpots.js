cc40bb26f51dc9e02f35ebd28b79fe6ef7de24e7
Joseph Volosin <josephvolosin@gmail.com>
2019-09-28 16:21:50 -0400

    Basic route for getSpots integration

---
+++ routes/getSpots.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../public/javascripts/db.js')
+/* GET users listing. */
+router.get('/getSpots', function(req, res, next) {
+    
+});
+
+module.exports = router;

e1982c147a57369fe3cc259293c3d81df0083fde
Joseph Volosin <josephvolosin@gmail.com>
2019-09-28 16:26:11 -0400

    Incorporating JS for DB queries

--- routes/getSpots.js
+++ routes/getSpots.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../public/javascripts/db.js')
-/* GET users listing. */
-router.get('/getSpots', function(req, res, next) {
-    
+
+// Request the newest 25 parking spots from the database
+router.get('/getSpots', function(req, res) {
+    var emptySpots = db.getSpots();
+    return res.send(emptySpots);
 });
 
 module.exports = router;

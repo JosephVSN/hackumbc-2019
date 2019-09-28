a81685e7d08a03367db0615d6e8f4fefe908ba6d
Joseph Volosin <josephvolosin@gmail.com>
2019-09-28 15:05:28 -0400

    Updates to client

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,6 +7,41 @@ var initMap = function() {
         document.getElementById('map'), {zoom: 16, center: umbcCoords});
 }
 
+// Gets the user's current geolocation (lat, long)
+// source: https://developers.google.com/maps/documentation/javascript/geolocation
+var getLocation = function() {
+    console.log("Getting current location..");
+    var pos = null;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+        });
+    }
+    return pos;
+}
+
+// Adds a new marker on the map with the user's location
+var addSpot = function() {
+    consolelog("Adding current location..");
+    
+    // 1. Get location
+    var pos = getLocation();
+    if (pos === null) {
+        console.log("ERROR: Couldn't get user's location");
+        return -1;
+    }
+    
+    // 2. Add to the map
+
+    // 3. Add to the SQL server
+
+    // 4. Refresh the list
+
+}
+
 $(document).ready(function() {
     console.log("Running!");
     initMap();
